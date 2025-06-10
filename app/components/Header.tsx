2a0610ec13ac472786207288ba855caee931d194
shaden <92798768+shadenGhasri@users.noreply.github.com>
2025-06-10 09:49:53 +0330

    add

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -84,7 +84,7 @@ export default function Header() {
         className="mx-auto flex max-w-7xl items-center justify-between py-6 px-5 lg:px-9"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <p className="text-2xl font-bold text-gray-800">Moltini</p>
           </a>
         </div>
@@ -199,7 +199,7 @@ export default function Header() {
         <div className="fixed inset-0 z-50" />
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5">
               <span className="text-2xl font-bold text-gray-800">Moltini</span>
             </a>
             <button
